3941668c4b8a5d264fb013735c6057153fdbd652
whatifitrains <victorathome@gmail.com>
2021-10-05 16:54:40 +0300

    proto for rpc key

--- jsonrpc-proto/src/lib.rs
+++ jsonrpc-proto/src/lib.rs
@@ -36,3 +36,69 @@ pub struct Application {
     pub proxy: ProxyEndpoint,
     pub active: bool,
 }
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RpcKey {
+    pub key_id: String,
+    pub key_hash: String,
+    pub apps: Vec<String>,
+    pub expires: u32,
+    pub active: bool,
+    pub quota_second: Option<u64>,
+    pub quota_minute: Option<u64>,
+    pub quota_hour: Option<u64>,
+    pub quota_day: Option<u64>,
+    pub quota_week: Option<u64>,
+    pub quota_month: Option<u64>,
+    pub quota_year: Option<u64>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RpcKeyAddRequest {
+    pub apps: Vec<String>,
+    pub expires: u32,
+    pub quota_second: Option<u64>,
+    pub quota_minute: Option<u64>,
+    pub quota_hour: Option<u64>,
+    pub quota_day: Option<u64>,
+    pub quota_week: Option<u64>,
+    pub quota_month: Option<u64>,
+    pub quota_year: Option<u64>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum RpcResponseStatus {
+    OK,
+    Failure,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum RpcKeyAction {
+    Add,
+    Get,
+    List,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RpcKeyAddResponse {
+    pub action: RpcKeyAction,
+    pub status: RpcResponseStatus,
+    pub key: String,
+    pub key_hash: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RpcKeyListResponse {
+    pub status: RpcResponseStatus,
+    pub action: RpcKeyAction,
+    pub keys: Vec<RpcKey>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct RpcKeyResponse {
+    pub status: RpcResponseStatus,
+    pub action: RpcKeyAction,
+    pub keys: Vec<RpcKey>,
+}
